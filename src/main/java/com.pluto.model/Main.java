0545e041ca6e7ab4c06a21ab96ecaff0efc2c959
manojplutotv <88347482+manojplutotv@users.noreply.github.com>
2021-11-16 16:02:33 -0700

    test commit

---
+++ src/main/java/com.pluto.model/Main.java
@@ -0,0 +1,22 @@
+package com.pluto.model;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+
+public class Main {
+
+  @Value("https://snowplow-hosted-assets.s3.amazonaws.com/third-party/ua-parser/regexes-latest.yaml")
+  private Resource regexYaml;
+
+  public void main(String[] args) {
+//    regexYaml.getInputStream()
+    System.out.println("testtt......."    );
+    if( true){
+      System.out.println(
+          " HELLLLOOOOOO..." );
+    }
+  }
+
+
+}
