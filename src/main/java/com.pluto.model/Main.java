7adceaef98472f5950c67cbbb9fe0e885326a7f9
manojplutotv <88347482+manojplutotv@users.noreply.github.com>
2021-11-16 16:32:29 -0700

    Update Main.java

--- src/main/java/com.pluto.model/Main.java
+++ src/main/java/com.pluto.model/Main.java
@@ -9,9 +9,9 @@ public class Main {
   @Value("https://snowplow-hosted-assets.s3.amazonaws.com/third-party/ua-parser/regexes-latest.yaml")
   private Resource regexYaml;
 
-  public void main(String[] args) {
+  public void main(String[] args    ) {
 //    regexYaml.getInputStream()
-    System.out.println("testtt.......     "    );
+    System.out.println("testtt.......     "          );
     if( true      ){
       System.out.println(
           " HELLLLOOOOOO..." );
